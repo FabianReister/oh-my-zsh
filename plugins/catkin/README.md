367a6051bbc8a76fd0811ef07d32f1ff87db836c
Fabian Reister <fabian.reister92@gmail.com>
2018-05-16 16:22:17 +0200

    adding catkin plugin

---
+++ plugins/catkin/README.md
@@ -0,0 +1,9 @@
+## catkin
+
+**Maintainer:** [@FabianReister](https://github.com/FabianReister)
+
+This plugin adds some useful aliases.
+
+### Usage
+
+See the [wiki](https://github.com/robbyrussell/oh-my-zsh/wiki/Plugin:catkin) for a list of aliases and functions provided by the plugin.
---
+++ plugins/catkin/catkin.plugin.zsh
@@ -0,0 +1,18 @@
+# ------------------------------------------------------------------------------
+# Description
+# -----------
+#
+#  Completion script for catkin (https://github.com/catkin/catkin_tools).
+#
+# ------------------------------------------------------------------------------
+# Authors
+# -------
+#
+#  * Fabian Reister (https://github.com/FabianReister)
+#
+# ------------------------------------------------------------------------------
+
+# aliases for catkin commands
+alias cb="catkin build"
+alias cbt="catkin build --this"
+alias cbtn="catkin build --this --no-deps"
