c723a7edb2ff7fb71850d227c7483e3e2393d6fb
Fabian Reister <fabian.reister92@gmail.com>
2018-06-09 15:07:51 +0200

    adding new aliases for "catkin run_tests" and "catkin config"

--- plugins/catkin/catkin.plugin.zsh
+++ plugins/catkin/catkin.plugin.zsh
@@ -12,7 +12,19 @@
 #
 # ------------------------------------------------------------------------------
 
+#############################
+#
 # aliases for catkin commands
+#
+#############################
+
+# catkin build
 alias cb="catkin build"
 alias cbt="catkin build --this"
 alias cbtn="catkin build --this --no-deps"
+# catkin run_tests
+alias crt="catkin run_tests"
+# catkin clean
+alias ccl="catkin clean"
+# catkin config
+alias cconf="catkin config"
